d5579c5c8c9cc597cb0a356b2cf8218a889a420a
Maxwell Knoxx <maxwell.knoxx@live.com>
2020-04-13 18:40:45 +0100

    first commit

---
+++ src/main/java/com/knoxx/fitnessearch/FitnessearchApplication.java
@@ -0,0 +1,13 @@
+package com.knoxx.fitnessearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knoxx/fitnessearch/FitnessearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoxx.fitnessearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
